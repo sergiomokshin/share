1e9936b0a4c77b60e4ec5061d7580bdbc4211c84
Sergio Mokshin <sergio.mokshin@gmail.com>
2012-03-24 19:55:53 -0300

    Padrao Visual

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-	has_many :posts
+	has_many :posts, :dependent => :destroy
 	
 	validates_presence_of :description
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 #encoding: utf-8
 
 class Post < ActiveRecord::Base
-	has_many :comments
-	has_many :links
-	has_many :attachments
+	has_many :comments, :dependent => :destroy
+	has_many :links, :dependent => :destroy
+	has_many :attachments, :dependent => :destroy
 	belongs_to :category
 	belongs_to :user
 	has_and_belongs_to_many :users
