41df0f6539ac66e98df9c92f7ce16ba4c29c0ba1
Sergio Mokshin <sergio.mokshin@gmail.com>
2012-03-28 19:32:48 -0300

    Configuracoes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, '251834431573170', '80d695620f24d870d79eb2ed1f6190ef'
+	#L provider :facebook, '251834431573170', '80d695620f24d870d79eb2ed1f6190ef'
+	provider :facebook, '419790658038540', '4a27c2093ff4fe51699f01adcf8ee711'
 
 end
 
